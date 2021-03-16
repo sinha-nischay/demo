6cb44e00fc5afa7ebca7ffdb97572ccdd662718f
sinha-nischay <nischaysinha1@gmail.com>
2021-03-16 16:28:06 +0530

    second commit

---
+++ second/java1.java
@@ -0,0 +1,11 @@
+public class java1 {
+public static void main(String[] args){
+System.out.println("Hello World!!");
+System.out.println("Hello World!!");
+System.out.println("Hello World!!");
+System.out.println("Heads Up!!");
+
+}
+
+
+}
