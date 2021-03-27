f225c382eb225fc76cad3a0033b63fab9608a1c0
sinha-nischay <nischaysinha1@gmail.com>
2021-03-27 16:54:30 +0530

    uploading war file

---
+++ Sample/src/main/java/com/nischay/app.java
@@ -0,0 +1,8 @@
+package com.nischay;
+
+public class app {
+	public String Sample() {
+		return "sample";
+	}
+
+}
---
+++ Sample/src/main/java/com/nischay/project.java
@@ -0,0 +1,7 @@
+package com.nischay;
+
+public class project {
+	public static void main(String [] args) {
+		System.out.println("This is turning to be an exciting thing!");
+	}
+}
